1514af73eef8809dd6921aa2bbd19891ae8ca027
unknown <mrbayezidhoshen@gmail.com>
2022-11-21 12:15:46 +0600

    Db connection and server run

---
+++ server/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import multer from "multer";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+/* Configuration */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+dotenv.config();
+app.use(express.json()); // to accept json
+app.use(helmet);
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// File Storage //
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+// DataBase Connection //
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Connected !");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Run Server
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server Running On Port ${PORT}`);
+});
