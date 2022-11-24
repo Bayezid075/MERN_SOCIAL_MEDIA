71930717ea3e25769ccedecb955b1e0ece06d557
unknown <mrbayezidhoshen@gmail.com>
2022-11-24 14:54:06 +0600

    basic routing and redux auth state handler created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./scenes/homePage/index";
+import LoginPage from "./scenes/loginPage/index";
+import ProfilePage from "./scenes/profilePage/index";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/profile/:userID" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 }
---
+++ client/src/state/index.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      (state.user = null), (state.token = null);
+    },
+    setFriend: (state, action) => {
+      if (state.user.friend) {
+        state.user.friend = action.payload.friend;
+      } else {
+        console.error("Friends does not exsist :( ");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPost = state.posts.map((post) => {
+        if (post._id === action.payload.post._id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPost;
+    },
+  },
+});
+
+export const { setMode, setFriend, setLogin, setLogout, setPost, setPosts } =
+  authSlice.actions;
+export default authSlice.reducer;
